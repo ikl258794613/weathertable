30daff525162eaf4121b7f9e7e41220b4e069ce5
侯奕任 <houyiren@houyirendeMacBook-Pro.local>
2022-07-24 15:02:40 +0800

    next use css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
-import WeatherTable from '@components/table/WeatherTable'
+import WeatherTable from './components/table/WeatherTable'
 import axios from 'axios'
-
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table'
+import { format } from 'date-fns'
 interface IErrorMessage {}
 
 interface IParameter {
@@ -12,6 +18,11 @@ interface IParameter {
 interface IWeatherElement {
   elementName: string
   elementValue: string
+}
+interface IReduceWeatherElement {
+  TEMP: string
+  WDSD: string
+  Weather: string
 }
 
 interface IFetchData {
@@ -27,6 +38,7 @@ interface IFetchData {
 }
 
 interface IWeatherTableData {
+  id: number
   county: string //  縣市
   area: string //  地區
   observedTime: Date | string //  觀測時間
@@ -40,6 +52,7 @@ export default function App() {
   const [errorMessage, setErrorMessage] = React.useState('')
   const [weatherData, setWeatherData] = React.useState<IWeatherTableData[]>([
     {
+      id: -1,
       county: '',
       area: '',
       observedTime: '',
@@ -48,6 +61,7 @@ export default function App() {
       windSpeed: '',
     },
   ])
+
   React.useEffect(() => {
     axios
       .get(
@@ -55,22 +69,43 @@ export default function App() {
       )
       .then((res) => {
         if (res.data.success) {
-          console.log(res?.data?.records?.location)
-          const targetData = res?.data?.records?.location.map(
-            (data: IFetchData) => {
-              const county = data.parameter.find((item) => {
-                return item.parameterName === 'CITY'
-              })
+          const locationData = res?.data?.records?.location
+          const targetData = locationData.map(
+            (data: IFetchData, index: number) => {
+              const parameter = data?.parameter.reduce(
+                (neededParameter: any, item) => {
+                  if (['CITY', 'TOWN'].includes(item.parameterName)) {
+                    neededParameter[item.parameterName] = item.parameterValue
+                  }
+                  return neededParameter
+                },
+                { CITY: '', TOWN: '' }
+              )
+              const weatherElements: IReduceWeatherElement =
+                data?.weatherElement.reduce(
+                  (neededElements: any, item) => {
+                    if (
+                      ['WDSD', 'TEMP', 'Weather'].includes(item.elementName)
+                    ) {
+                      neededElements[item.elementName] = item.elementValue
+                    }
+                    return neededElements
+                  },
+                  { WDSD: '', TEMP: '', Weather: '' }
+                )
+              // console.log(weatherElements)
               return {
-                county: county?.parameterValue,
-                area: '',
-                observedTime: '',
-                weather: '',
-                temperature: '',
-                windSpeed: '',
+                id: index,
+                county: parameter?.CITY,
+                area: parameter?.TOWN,
+                observedTime: data?.time?.obsTime,
+                weather: weatherElements?.Weather,
+                temperature: weatherElements?.TEMP,
+                windSpeed: weatherElements?.WDSD,
               }
             }
           )
+          setWeatherData(targetData)
         } else {
           setErrorMessage('氣象局api連線不穩請稍後再試')
         }
@@ -80,12 +115,100 @@ export default function App() {
         console.log(error)
       })
   }, [])
-
+  const tableWeatherData = React.useMemo(() => weatherData, [])
+  const columnHelper = createColumnHelper<IWeatherTableData>()
+  const columns = [
+    columnHelper.accessor('county', {
+      header: (info) => {
+        return (
+          <>
+            <span>縣市</span>
+          </>
+        )
+      },
+      cell: (info) => info.getValue(),
+      footer: '',
+    }),
+    columnHelper.accessor('area', {
+      header: (info) => {
+        return (
+          <>
+            <span>地區</span>
+          </>
+        )
+      },
+      cell: (info) => info.getValue(),
+      footer: '',
+    }),
+    columnHelper.accessor('observedTime', {
+      header: (info) => {
+        return (
+          <>
+            <span>觀測時間</span>
+          </>
+        )
+      },
+      cell: (info) => {
+        return format(new Date(info.getValue()), 'yyyy/MM/dd HH:mm')
+      },
+      footer: '',
+    }),
+    columnHelper.accessor('weather', {
+      header: (info) => {
+        return (
+          <>
+            <span>天氣</span>
+          </>
+        )
+      },
+      cell: (info) => {
+        return Number(info.getValue()) === -99 ? '儀器故障' : info.getValue()
+      },
+      footer: '',
+    }),
+    columnHelper.accessor('temperature', {
+      header: (info) => {
+        return (
+          <>
+            <span>溫度</span>
+          </>
+        )
+      },
+      cell: (info) => {
+        return Number(info.getValue()) === -99 ? '儀器故障' : info.getValue()
+      },
+      footer: '',
+    }),
+    columnHelper.accessor('windSpeed', {
+      header: (info) => {
+        return (
+          <>
+            <span>風速</span>
+          </>
+        )
+      },
+      cell: (info) => {
+        return Number(info.getValue()) === -99 ? '儀器故障' : info.getValue()
+      },
+      footer: '',
+    }),
+    columnHelper.accessor('id', {
+      header: '',
+      cell: (info) => {
+        return (
+          <>
+            <button onClick={() => console.log(info)}>test</button>
+          </>
+        )
+      },
+      footer: '',
+    }),
+  ]
   return (
     <>
-      <div className='w-full h-full flex flex-row justify-center p-10 bg-gradient-to-b from-[#e6f7ff] to-[#FFFFFF]'>
-        <div className='w-[80%]'>
-          {/* <WeatherTable data={[]} columns={[]} /> */}
+      <div className='w-full h-[100vh] flex flex-row justify-center p-10 bg-gradient-to-b from-[#70bde0] to-[#FFFFFF]'>
+        <div className='w-[80%] max-h-[75%]'>
+          <WeatherTable data={tableWeatherData} columns={columns} />
         </div>
       </div>
     </>
